675522879c083a74de88843afd702d202bc480e8
Maria Shugrina <shumash@gmail.com>
2019-11-07 20:34:15 -0500

    Allowed including specific frames, as required for test set.

--- creativeflow/run_unittests.sh
+++ creativeflow/run_unittests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR/.."
 
-python -m unittest discover -v -s ./tests -p '*_test.py'
+python -m unittest discover -v -s ./creativeflow/tests -p '*_test.py'
